91c35e42a52daac1fa2c46e7b630dc63fa504a6d
hoang <lhoang17062000@gmail.com>
2020-10-12 00:43:49 +0700

    add suggestion target

--- src/sample/MainController.java
+++ src/sample/MainController.java
@@ -1,11 +1,11 @@
 package sample;
 
-//import com.gtranslate.Audio;
-//import com.gtranslate.Language;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -14,12 +14,12 @@ import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
-//import javazoom.jl.decoder.JavaLayerException;
-
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class MainController {
+public class MainController implements Initializable {
 
     @FXML
     private TextField Target;
@@ -27,7 +27,6 @@ public class MainController {
     private TextField Explain;
     @FXML
     private ListView<String> Listview;
-
 
     public void textAction(ActionEvent Event) throws IOException {
         if(Listview.getItems().size() != 0) {
@@ -41,10 +40,19 @@ public class MainController {
         Target.setOnKeyPressed(keyEvent -> {
             if (keyEvent.getCode().equals(KeyCode.ENTER)) {
                 Explain.setText(a.dictionaryManagement.dictionaryLookup(target));
-
+                ArrayList<String> searcher_all = a.dictionaryManagement.dictionarySearcher(""); // search tat ca cac tu
                 ArrayList<String> searcher = a.dictionaryManagement.dictionarySearcher(target);
-                for (String temp : searcher){
-                    Listview.getItems().add(temp);
+                for (String temp : searcher_all){
+                    //Listview.getItems().add(temp);
+                    if (target.length() == 1) {
+                        for (String m:searcher) {
+                            Listview.getItems().add(m);
+                        }
+                        break;
+                    } else
+                        if(lcs(target, temp).length() >= target.length()-1) {
+                            Listview.getItems().add(temp);
+                        }
                 }
             }
         });
@@ -59,22 +67,6 @@ public class MainController {
         });
     }
 
-
-//    public void speak(ActionEvent Event) throws IOException, JavaLayerException {
-//
-//        Audio audio = Audio.getInstance();
-//        InputStream sound = null;
-//        try {
-//            sound = audio.getAudio(Target.getText(), Language.ENGLISH);
-//        } catch (IOException ex) {
-//            Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//        try {
-//            audio.play(sound);
-//        } catch (JavaLayerException ex) {
-//            Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//    }
 
     public void add(ActionEvent event) throws IOException {
         Parent add_gui = FXMLLoader.load(getClass().getResource("addWordGUI.fxml"));
@@ -100,8 +92,71 @@ public class MainController {
         window.show();
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        DictionaryCommandline a = new DictionaryCommandline();
+        try {
+            a.dictionaryManagement.insertFromFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ArrayList<String> listTarget = a.dictionaryManagement.dictionarySearcher("");
+        for (String m : listTarget) {
+            Listview.getItems().add(m);
+        }
+
+        Listview.setOnMouseClicked(mouseEvent -> {
+            ObservableList<String> list1 = Listview.getSelectionModel().getSelectedItems();
+            for (String m : list1) {
+                Target.setText(m);
+                Explain.setText(a.dictionaryManagement.dictionaryLookup(m));
+            }
+        });
+    }
+
+//    private class MyTasks extends Task {
+//
+//        @Override
+//        protected Object call() throws Exception {
+//            isCanQuery = false;
+//            System.out.println(isCanQuery);
+//            Thread.sleep(2000);
+//            isCanQuery = true;
+//            System.out.println(isCanQuery);
+//            return null;
+//        }
+//    }
+
+    public String lcs(String str1, String str2) {
+        int l1 = str1.length();
+        int l2 = str2.length();
+
+        int[][] arr = new int[l1 + 1][l2 + 1];
+
+        for (int i = l1 - 1; i >= 0; i--) {
+            for (int j = l2 - 1; j >= 0; j--) {
+                if (str1.charAt(i) == str2.charAt(j))
+                    arr[i][j] = arr[i + 1][j + 1] + 1;
+                else
+                    arr[i][j] = Math.max(arr[i + 1][j], arr[i][j + 1]);
+            }
+        }
+
+        int i = 0, j = 0;
+        StringBuffer sb = new StringBuffer();
+        while (i < l1 && j < l2) {
+            if (str1.charAt(i) == str2.charAt(j)) {
+                sb.append(str1.charAt(i));
+                i++;
+                j++;
+            } else if (arr[i + 1][j] >= arr[i][j + 1])
+                i++;
+            else
+                j++;
+        }
+
+        return sb.toString();
+
+    }
+
 }
-
-
-
-
